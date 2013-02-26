34f461ca2c2797046caa1ad0e31956f9827dfe98
mjason <tywf91@gmail.com>
2013-02-26 14:45:29 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.12'
+
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# To use Jbuilder templates for JSON
+gem 'jbuilder'
+
+# 存储
+gem "mongoid", "~> 3.0.0"
+
+# user
+gem "devise"
+gem "cancan"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestclass::Application.config.secret_token = '44997d67c70f1b9a867a207f9115f7058e85e8662363449aa98e924d80d5b63df0f8d4412aa85e027cba3c77b2a287473c4b6d7ed71e21924530ca3925a973c1'
