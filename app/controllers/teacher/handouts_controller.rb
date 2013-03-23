6d754929f95589c587b6fa49e559160a64abab9d
mjason <tywf91@gmail.com>
2013-03-23 19:00:05 +0800

    提交在线转换代码

--- app/controllers/teacher/handouts_controller.rb
+++ app/controllers/teacher/handouts_controller.rb
@@ -1,7 +1,13 @@
 class Teacher::HandoutsController < TeacherController
 
   def create
-    
+    handout = current_user.teach_courses.find(params[:course_id]).handouts.new(params[:handout])
+    if handout.save
+      handout.to_pdf
+      handout.to_swf
+      handout.save
+      redirect_to teacher_course_path(params[:course_id])
+    end
   end
-  
+
 end
